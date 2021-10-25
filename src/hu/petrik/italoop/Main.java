58f16a92ff81f676046cd1871153739ef5aeed78
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-26 00:37:17 +0200

    Futtathato osztály dokumentálva

--- src/hu/petrik/italoop/Main.java
+++ src/hu/petrik/italoop/Main.java
@@ -5,10 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Bor b = new Bor("Teszt Bor", 1154);
-        System.out.println(b.toString());
-
         Aszu a = new Aszu(1342, 6);
-        System.out.println(a.toString());
 
+        Futtathato.kiirBor(a);
+        Futtathato.kiirBor(b);
     }
 }
