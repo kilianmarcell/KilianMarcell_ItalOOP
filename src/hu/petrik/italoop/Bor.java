82de48bf227007c4628eda6e05173ca32af5872d
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-25 22:30:04 +0200

    Bor osztály konstruktora kész, getterek/setterek és toString metódusa kész

--- src/hu/petrik/italoop/Bor.java
+++ src/hu/petrik/italoop/Bor.java
@@ -3,4 +3,30 @@ package hu.petrik.italoop;
 public class Bor {
     private String fajta;
     private int evjarat;
+
+    public Bor(String fajta, int evjarat) {
+        this.fajta = fajta;
+        this.evjarat = evjarat;
+    }
+
+    public String getFajta() {
+        return fajta;
+    }
+
+    public int getEvjarat() {
+        return evjarat;
+    }
+
+    public void setFajta(String fajta) {
+        this.fajta = fajta;
+    }
+
+    public void setEvjarat(int evjarat) {
+        this.evjarat = evjarat;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bor fajtája: %s\nBor évjárata: %d", fajta, evjarat);
+    }
 }
--- src/hu/petrik/italoop/Main.java
+++ src/hu/petrik/italoop/Main.java
@@ -3,5 +3,7 @@ package hu.petrik.italoop;
 public class Main {
 
     public static void main(String[] args) {
+        Bor b = new Bor("Teszt Bor", 1154);
+        System.out.println(b.toString());
     }
 }
