1f531c823e0c0656932b587fdb2163931633d536
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-26 00:37:00 +0200

    Futtathato osztály dokumentálva

--- src/hu/petrik/italoop/Futtathato.java
+++ src/hu/petrik/italoop/Futtathato.java
@@ -2,10 +2,16 @@ package hu.petrik.italoop;
 
 public class Futtathato {
 
+    /**
+     * @param args Ez a metódus a függvény alap metódusa
+     */
     public static void main(String[] args) {
 
     }
 
+    /**
+     * @param bor A paraméternek megadott pédányosított Bor-t kiírja (meghívja a toString() metódusát
+     */
     public static void kiirBor(Bor bor) {
         System.out.println(bor.toString());
     }
