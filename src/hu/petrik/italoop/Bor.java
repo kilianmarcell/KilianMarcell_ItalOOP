e36f25bc687a104e1460c8ff40173c7fa2f405ba
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-25 22:22:25 +0200

    Bor osztály és attribútumai létrehozva

---
+++ src/hu/petrik/italoop/Bor.java
@@ -0,0 +1,6 @@
+package hu.petrik.italoop;
+
+public class Bor {
+    private String fajta;
+    private int evjarat;
+}
--- src/hu/petrik/italoop/Main.java
+++ src/hu/petrik/italoop/Main.java
@@ -3,6 +3,5 @@ package hu.petrik.italoop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
