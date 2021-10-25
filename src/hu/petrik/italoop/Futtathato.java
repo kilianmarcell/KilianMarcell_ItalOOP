cdde400ccc184617c18dd0e9217771160a5e3871
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-25 22:55:37 +0200

    Futtathato osztály kész, kiirBor metódusa kész, metódus tesztelve - működik

---
+++ src/hu/petrik/italoop/Futtathato.java
@@ -0,0 +1,12 @@
+package hu.petrik.italoop;
+
+public class Futtathato implements Runnable {
+    @Override
+    public void run() {
+
+    }
+
+    public static void kiirBor(Bor b) {
+        System.out.println(b.toString());
+    }
+}
